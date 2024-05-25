74e531a23af48056f3c3e239ead499dd35dccb42
Philip Brown <houseofyair@gmail.com>
2024-05-24 20:47:56 -0400

    Add PWA Functionality (#1)
    - install @nuxt/pwa package
    - include logo files
    - include robot.txt
    - update api file for updated records

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,52 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],  
+  pwa: {
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: "Nuxt Vite PWA",
+      short_name: "NuxtVitePWA",
+      theme_color: "#ffffff",
+      
+      icons: [
+        {
+          src: "pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png",
+        },
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+  },
   app: {
+    baseURL: '/gh-planeswalker-list/',
     head: {
       title: 'Planeswalker List',
-      viewport: 'width=device-width, initial-scale=1',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      htmlAttrs: {
+        lang: 'en'
+      },      
     }
-  }
-})
+  },
+  imports: {
+    autoImport: true,
+  },
+});
