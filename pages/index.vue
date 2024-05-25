f6473b2f8bd85dadacbed57d725ee27b1424d21b
houseofyair <houseofyair@gmail.com>
2024-05-24 20:43:38 -0400

    Add PWA Functionality - install @nuxt/pwa package - include logo files - include robot.txt - update api file for updated records

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <table class="w-full table-auto border-slate-500">
+      <thead>
+        <th class="text-left pl-2 py-1">Name</th>
+        <th class="text-left pl-2 py-1">Set</th>
+      </thead>
+      <tbody>
+        <tr v-for="card in data.cards" class="border-t border-slate-600 even:bg-amber-100 odd:bg-slate-200">
+          <td class="text-left pl-2 py-1 border-slate-600 font-normal">
+            {{ card.name }}
+          </td>
+          <td class="text-left pl-2 border-slate-600 font-light">
+            {{ card.set }}
+          </td>
+        </tr>
+      </tbody>
+      <caption class="caption-bottom">
+        Last Updated on
+        {{ data.lastUpdatedDate }}
+      </caption>
+    </table>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/planeswalkers");
+</script>
