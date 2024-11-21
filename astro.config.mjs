71245007e8995e3ef5296d31be982ffff40398e4
houseofyair <houseofyair@gmail.com>
2024-11-20 23:22:10 -0500

    Fix Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://houseofyair.github.io',
+	base: 'gh-planeswalker-list',
   integrations: [
     UnoCSS({
       injectReset: true,
