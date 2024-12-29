c3a3c21da0386fa2bd403acc97162fc6fe691492
Philip Brown <houseofyair@gmail.com>
2024-12-28 19:20:08 -0500

    Update Design

--- src/data/scryfall.api.ts
+++ src/data/scryfall.api.ts
@@ -1,10 +1,14 @@
+import type { boolean } from "astro:schema";
 import collected from "./current-planeswalkers.json";
 
 export interface PlaneswalkerCard {
 	display_name: string;
+	collector_number: string;
 	set: string;
+	set_name: string;
 	set_icon: string;
-	price: number;
+	price: number | null;
+	image_src: string;
 	isFoil: boolean;
 }
 
@@ -33,29 +37,50 @@ export default async function requestHttp() {
 		.then((r) => r.json())
 		.then((i) => i.data);
 
-	const getSetIcon = (code: string) => {
-		let set = sets.find((i: { code: string }) => i.code === code);
-		return set ? set.icon_svg_uri.substr(0, set.icon_svg_uri.indexOf("?")) : null;
+	const getCardImage = (card: any, isFlipCard: boolean): string => {
+		if (isFlipCard) {
+			let planeswalker = card.card_faces.find((i: any) => i.type_line.indexOf("Planeswalker") > -1);
+			return planeswalker.image_uris.border_crop;
+		}
+		return card.image_uris.border_crop;
+	};
+
+	const getCardName = (card: any, isFlipCard: boolean): string => {
+		if (isFlipCard) {
+			let planeswalker = card.card_faces.find((i: any) => i.type_line.indexOf("Planeswalker") > -1);
+			return planeswalker.name;
+		}
+
+		return card.name;
 	};
 
 	const getPriceString = (prices: { usd: any; usd_foil: any }) => {
 		if (prices.usd) return [prices.usd, false];
 		if (prices.usd_foil) return [prices.usd_foil, true];
-		return ["---", false];
+		return [null, false];
+	};
+
+	const getSetIcon = (code: string) => {
+		let set = sets.find((i: { code: string }) => i.code === code);
+		return set ? set.icon_svg_uri.substr(0, set.icon_svg_uri.indexOf("?")) : null;
 	};
 
 	const data: PlaneswalkerCard[] = cards
 		.filter((i) => !collected.find((m) => m.name === i.name && !m.isReprint))
 		.map((card) => {
 			let pricing = getPriceString(card.prices);
+			let isFlipCard = ["transform", "modal_dfc"].includes(card.layout) as boolean;
 
 			return {
 				name: card.name,
-				display_name: card.name.indexOf("//") > -1 ? card.name.split("//")[1].trim() : card.name,
+				collector_number: card.collector_number.padStart(3, "0"),
+				display_name: getCardName(card, isFlipCard),
 				set: card.set.toUpperCase(),
+				set_name: card.set_name,
 				set_icon: getSetIcon(card.set),
 				price: pricing[0],
 				isFoil: pricing[1],
+				image_src: getCardImage(card, isFlipCard),
 			};
 		});
 
